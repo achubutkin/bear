d85543d122dd7e43a412b7fc892b925a61613e22
a.chubutkin <a.chubutkin@gmail.com>
2016-10-25 17:18:38 +0600

    Create singleton BearApp.

---
+++ App_Code/BearApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+/// <summary>
+/// Summary description for BearApp
+/// </summary>
+public class BearApp
+{
+    private static BearApp _instance = new BearApp();
+    
+    public static BearApp Instance 
+    {
+        get { return _instance; }
+    }
+
+    private BearApp()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
